07763a1929f368f8ea2e6d339a7f417f19578247
aronand <71503746+aronand@users.noreply.github.com>
2022-12-10 14:16:57 +0200

    Adds docstrings to BlenderObjectProtocol

--- src/blenderobjectprotocol.py
+++ src/blenderobjectprotocol.py
@@ -2,6 +2,7 @@ from typing import Any, Protocol
 
 
 class BlenderObjectProtocol(Protocol):
+    """Defines/recreates an interface similar to bpy.types.Object"""
     @property
     def name(self) -> str: ...
 
@@ -12,7 +13,7 @@ class BlenderObjectProtocol(Protocol):
     def data(self) -> Any: ...  # TODO: Switch to BlenderMeshProtocol once implemented
 
     @property
-    def modifiers(self) -> Any: ...  # TODO: What does modifiers actually return?
+    def modifiers(self) -> Any: ...  # TODO: What does modifiers actually return? -> bpy_prop_collection
 
     @property
-    def material_slots(self) -> Any: ...  # TODO: What does material_slots actually return?
+    def material_slots(self) -> Any: ...  # TODO: What does material_slots actually return? -> bpy_prop_collection
