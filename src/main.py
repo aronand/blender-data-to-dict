b2a750cfd19c9ffd8de2e037df1d968a2e51c952
aronand <71503746+aronand@users.noreply.github.com>
2022-12-11 20:06:35 +0200

    Changes main into a class Main

--- src/main.py
+++ src/main.py
@@ -6,28 +6,46 @@ import sys
 from typing import Any
 
 
-def main(root_path: str):
-    from blenderdata import blender_object_generator
+class Main:
+    def __init__(self):
+        self.__root: str | None = None  # Use the property and the setter instead of directly accessing
+        self.objects: dict[str, Any] = {}
 
-    objects_dict: dict[str, Any] = {}
-    for obj in blender_object_generator(bpy.data.objects):
-        obj_dict: dict[str, Any] = obj.dict
-        obj_name: str = list(obj_dict.keys())[0]
-        logging.debug(f"Adding object '{obj_name}'")
-        objects_dict |= obj_dict
+    @property
+    def root(self) -> str:
+        return self.__root
 
-    json_path: str = os.path.join(root_path, "output.json")
-    json_contents: dict[str, dict] = {"objects": objects_dict}
-    logging.debug(f"Writing contents to {json_path}")
-    with open(json_path, "w") as output:
-        json.dump(json_contents, output, indent=2)
+    @root.setter
+    def root(self, path: str):
+        if not os.path.isdir(path):
+            raise FileNotFoundError(f"Couldn't set root path, {path} not found")
+        self.__root = path
+
+    def __get_root_path(self):
+        """Gets the root path from args and appends it to sys.path"""
+        idx: int = sys.argv.index("--")
+        self.root = sys.argv[idx + 1]  # TODO: Use argparse instead if possible?
+        logging.debug(f"Root path is {self.root}")
+        sys.path.append(self.root)
+
+    def run(self):
+        self.__get_root_path()
+        from blenderdata import blender_object_generator
+
+        for obj in blender_object_generator(bpy.data.objects):
+            obj_dict: dict[str, Any] = obj.dict
+            obj_name: str = list(obj_dict.keys())[0]
+            logging.debug(f"Adding object '{obj_name}'")
+            self.objects |= obj_dict
+
+        json_path: str = os.path.join(self.root, "output.json")
+        json_contents: dict[str, dict] = {"objects": self.objects}
+        logging.debug(f"Writing contents to {json_path}")
+        with open(json_path, "w") as output:
+            json.dump(json_contents, output, indent=2)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     logging.debug("Starting script")
-    idx: int = sys.argv.index("--")
-    root: str = sys.argv[idx + 1]
-    logging.debug(f"Root path is {root}")
-    sys.path.append(root)
-    main(root)
+    Main().run()
