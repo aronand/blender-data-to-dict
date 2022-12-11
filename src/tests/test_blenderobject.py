0739d030fd61a1d9ae0371231ace10b20170ae73
aronand <71503746+aronand@users.noreply.github.com>
2022-12-12 00:20:49 +0200

    Adds BlenderDataFactory
    BlenderDataFactory can be used to generate BlenderObjects and BlenderMeshes.
    
    Takes BlenderObject generating methods and functions to itself.
    
    To be extended in the future.

--- src/tests/test_blenderobject.py
+++ src/tests/test_blenderobject.py
@@ -1,7 +1,7 @@
 from typing import Any
 from unittest import TestCase
 
-from blenderdata import BlenderMesh, BlenderObject, blender_object_generator, BlenderObjectProtocol
+from blenderdata import BlenderMesh, BlenderObject, BlenderObjectProtocol
 
 
 class TestBlenderObject(TestCase):
@@ -29,15 +29,6 @@ class TestBlenderObject(TestCase):
             self.assertEqual(None, self.blender_object.data)
             self.assertEqual([], self.blender_object.modifiers)
             self.assertEqual([], self.blender_object.material_slots)
-        with self.subTest("Object can be initialized by another object compliant with BlenderObjectProtocol"):
-            bl_obj: BlenderObject = BlenderObject()
-            bl_obj.name = "test"
-            bl_obj.type = "MESH"
-            bl_obj.data = self.mesh
-            self.blender_object.init_from_object(bl_obj)
-            self.assertEqual("test", self.blender_object.name)
-            self.assertEqual("MESH", self.blender_object.type)
-            self.assertEqual(self.mesh, self.blender_object.data)
 
     def test_type_property(self):
         with self.subTest("Setter: Type must be str (raises TypeError)"):
@@ -67,21 +58,3 @@ class TestBlenderObject(TestCase):
             self.mock_dict[bl_obj.name]["type"] = bl_obj.type
             self.mock_dict[bl_obj.name]["data"] = bl_obj.data.name
             self.assertEqual(self.mock_dict, bl_obj.dict)
-
-
-class TestBlenderObjectGenerator(TestCase):
-    objects: list[BlenderObject]
-    names: list[str] = [f"Test{i}" for i in range(9)]
-
-    def setUp(self) -> None:
-        self.objects = []
-        for name in self.names:
-            blender_object: BlenderObject = BlenderObject()
-            blender_object.name = name
-            blender_object.type = "EMPTY"
-            self.objects.append(blender_object)
-
-    def test_generator(self):
-        with self.subTest("Names match"):
-            names: list[str] = [obj.name for obj in blender_object_generator(self.objects)]
-            self.assertEqual(names, self.names)
