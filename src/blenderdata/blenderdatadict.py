73fac5e0434e518fc9b356bb84f03152670b3ebb
aronand <71503746+aronand@users.noreply.github.com>
2022-12-12 01:18:41 +0200

    Adds basic framework for getting the correct dict

--- src/blenderdata/blenderdatadict.py
+++ src/blenderdata/blenderdatadict.py
@@ -1,7 +1,28 @@
-from typing import Any
+from typing import Any, Callable
+
+from .blendermeshprotocol import BlenderMeshProtocol
+from .blenderobjectprotocol import BlenderObjectProtocol
 
 
 class BlenderDataDict:
     @classmethod
     def get_dict(cls, obj: Any) -> dict:
-        return {}
+        __types: dict[str, Callable[..., dict[str, Any]]] = {
+            "Object": cls.get_object_dict,
+            "Mesh": cls.get_mesh_dict
+        }
+        data: dict = {}
+        obj_type: str = type(obj).__name__
+        for T, func in __types.items():
+            if not obj_type.endswith(T):
+                continue
+            data = func(obj)
+        return data
+
+    @classmethod
+    def get_object_dict(cls, obj: BlenderObjectProtocol) -> dict[str, Any]:
+        pass
+
+    @classmethod
+    def get_mesh_dict(cls, mesh: BlenderMeshProtocol) -> dict[str, Any]:
+        pass
--- src/tests/test_blenderdatadict.py
+++ src/tests/test_blenderdatadict.py
@@ -1,8 +1,26 @@
 from unittest import TestCase
 
-from blenderdata import BlenderDataDict
+from blenderdata import BlenderDataDict, BlenderMesh, BlenderObject
 
 
 class TestBlenderDataDict(TestCase):
+    blender_object: BlenderObject
+    mesh: BlenderMesh
+
+    def setUp(self) -> None:
+        self.blender_object = BlenderObject()
+        self.mesh = BlenderMesh()
+
     def test_get_dict(self):
-        self.assertEqual({}, BlenderDataDict.get_dict(None))
+        with self.subTest("None returns an empty dict"):
+            self.assertEqual({}, BlenderDataDict.get_dict(None))
+        with self.subTest("BlenderObject returns a correct dict"):
+            self.fail()
+        with self.subTest("BlenderMesh returns a correct dict"):
+            self.fail()
+
+    def test_get_object_dict(self):
+        self.fail()
+
+    def test_get_mesh_dict(self):
+        self.fail()
