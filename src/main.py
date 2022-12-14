1094690a5db6f6a3a09ce5bbb989577cf1f120a9
aronand <71503746+aronand@users.noreply.github.com>
2022-12-14 20:52:56 +0200

    Merges everything into main.py

--- src/main.py
+++ src/main.py
@@ -1,78 +1,61 @@
 import bpy
-import json
-import logging
-import os.path
-import sys
-import time
 from typing import Any
 
 
-class Main:
-    def __init__(self):
-        self.__root: str | None = None  # Use the property and the setter instead of directly accessing
-        self.objects: dict[str, Any] = {}
-        self.meshes: dict[str, Any] = {}
-        self.materials: dict[str, Any] = {}
-        self.images: dict[str, Any] = {}
-
-    @property
-    def root(self) -> str:
-        return self.__root
-
-    @root.setter
-    def root(self, path: str):
-        if not os.path.isdir(path):
-            raise FileNotFoundError(f"Couldn't set root path, {path} not found")
-        self.__root = path
-
-    def __get_root_path(self):
-        """Gets the root path from args and appends it to sys.path"""
-        idx: int = sys.argv.index("--")
-        self.root = sys.argv[idx + 1]  # TODO: Use argparse instead if possible?
-        logging.debug(f"Root path is {self.root}")
-        sys.path.append(self.root)
-
-    def run(self):
-        self.__get_root_path()
-        from blenderdatadict import BlenderDataDict
-
-        logging.info("Getting objects")
+class BlenderData:
+    @classmethod
+    def objects_dict(cls) -> dict[str, Any]:
+        obj: bpy.types.Object
+        objects: dict[str, Any] = {}
         for obj in bpy.data.objects:
-            logging.debug(f"Adding object '{obj.name}'")
-            self.objects |= BlenderDataDict.get_dict(obj)
+            data: str | None = (None if obj.data is None else obj.data.name)
+            objects[obj.name] = {
+                "type": obj.type,
+                "data": data,
+                "modifiers": [mod.type for mod in obj.modifiers],
+                "material_slots": [mat.name for mat in obj.material_slots]
+            }
+        return objects
 
-        logging.info("Getting meshes")
+    @classmethod
+    def mesh_dict(cls) -> dict[str, Any]:
+        mesh: bpy.types.Mesh
+        meshes: dict[str, Any] = {}
         for mesh in bpy.data.meshes:
-            logging.debug(f"Adding mesh '{mesh.name}'")
-            self.meshes |= BlenderDataDict.get_dict(mesh)
+            meshes[mesh.name] = {}
+        return meshes
 
-        logging.info("Getting materials")
+    @classmethod
+    def materials_dict(cls) -> dict[str, Any]:
+        material: bpy.types.Material
+        materials: dict[str, Any] = {}
         for material in bpy.data.materials:
-            logging.debug(f"Adding material '{material.name}'")
-            self.materials |= BlenderDataDict.get_dict(material)
+            materials[material.name] = {}
+        return materials
 
-        logging.info("Getting images")
+    @classmethod
+    def images_dict(cls) -> dict[str, Any]:
+        image: bpy.types.Image
+        images: dict[str, Any] = {}
         for image in bpy.data.images:
-            logging.debug(f"Adding image '{image.name}'")
-            self.images |= BlenderDataDict.get_dict(image)
+            images[image.name] = {}
+        return images
 
-        json_path: str = os.path.join(self.root, "output.json")
-        json_contents: dict[str, dict] = {
-            "objects": self.objects,
-            "meshes": self.meshes,
-            "materials": self.materials,
-            "images": self.images
+    @classmethod
+    def dict(cls) -> dict[str, Any]:
+        """Returns everything in a single dictionary. Must be defined after all the classmethods it calls!"""
+        return {
+            "objects": cls.objects_dict(),
+            "meshes": cls.mesh_dict(),
+            "materials": cls.materials_dict(),
+            "images": cls.images_dict()
         }
-        logging.info(f"Writing contents to {json_path}")
-        with open(json_path, "w") as output:
-            json.dump(json_contents, output, indent=2)
+
+
+def main() -> None:
+    data: dict[str, Any] = BlenderData.dict()
+    print(data)
 
 
 if __name__ == "__main__":
-    start_time: float = time.perf_counter()
-    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s: %(levelname)s - %(message)s")
-    logging.info("Starting script")
-    Main().run()
-    elapsed_time: float = time.perf_counter() - start_time
-    logging.info(f"Took {elapsed_time:0.2f} seconds")
-
+    main()
